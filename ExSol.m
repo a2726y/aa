7933601ffc3623179fc94695507f8c354ecf0bf6
a2726y <154469090+a2726y@users.noreply.github.com>
2024-11-15 08:08:35 -0500

    Add files via upload

---
+++ ExSol.m
@@ -0,0 +1,25 @@
+function T = ExSol(alpha,t,x_espa)
+alfa = alpha;
+dx=0.0001;
+T0=100;
+Lx=1;
+dt=0.05;
+m_max=10;
+t_max=t;
+
+%Main computation
+Nx = round(Lx/dx)+1;
+Nt =round(t_max/dt);
+suma=0;
+x=linspace(0,Lx,x_espa);
+for nt=1:Nt
+    t=nt*dt;
+
+    for m=1:m_max
+    suma = suma+sin((2*m-1)*pi*x)*exp(-alfa*((2*m-1)^2)*(pi^2)*t)/(2*m-1);
+    end
+
+    T=T0*(1-(4/pi)*suma);
+    suma=0;
+end
+end
